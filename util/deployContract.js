b56a520bad03303ed52cadc7e447ac80707f2f0b
john-boid <john@boid.com>
2019-11-26 21:11:03 -0600

    deploy

--- util/deployContract.js
+++ util/deployContract.js
@@ -1,14 +1,22 @@
 var exec = require('child-process-promise').exec
 const env = require('../.env')
 const accounts = require('./accounts')
+const err = (error) => console.error(error.toString())
+
 async function init(contractName){
   try {
+    var result
     if (!contractName) throw(new Error("Missing contract name!"))
-    const result = await exec(` cleos -u ${env.endpoints[env.network].rpc} wallet unlock --password ${env.walletPW}`)
-    console.log(result)
-  } catch (error) {
-    console.error(error.toString())
-  }
+    result = await exec(`wallet unlock --password ${env.walletPW}`)
+    .catch(err)
+    if (result) console.log(result.stdout)
+    result = await exec(`
+    cleos -u ${env.endpoints[env.network].rpc} set code ${accounts[env.network].contractName} ../_compiled_contracts/${contractName}/jungle/${contractName}.wasm
+    `)
+    if (result) console.log(result.stdout)
+
+
+  } catch (error) { err(error) }
 }
 
 
